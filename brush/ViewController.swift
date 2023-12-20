67fd61e2f74eec79580b377ea25b38c249acef57
Ksenia <maricheva.ksenia@yandex.ru>
2023-12-20 21:51:42 +0100

    brash

--- brush/ViewController.swift
+++ brush/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var brushLabel: UILabel!
+    @IBOutlet weak var brashButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func plasOne(_ sender: Any) {
+        var a = Int(brushLabel.text!)
+        a! += 1
+        brushLabel.text = "\(a!)"
+        
+    }
+    
 }
 
